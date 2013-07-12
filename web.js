d58aa59825c93b28bddf0ecdcc428a2899cc99c9
Yatharth Saraf <yatharthsaraf@gmail.com>
2013-07-12 06:30:26 +0000

    Read from file

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require('express');
+var fs = require('fs');
+
+var infile = "index.html";
+var buf = fs.readFileSync(infile);
+var str = buf.toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
