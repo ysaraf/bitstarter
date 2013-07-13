28ad766a71c2f09fec5413ca5158f337ffede7c7
Yatharth Saraf <yatharthsaraf@gmail.com>
2013-07-13 05:15:08 +0000

    Add url

--- grader.js
+++ grader.js
@@ -24,6 +24,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
 
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
@@ -37,23 +39,24 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var loadHtmlFile = function(htmlfile) {
+    return fs.readFileSync(htmlfile);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(htmlString, checksfile) {
+    $ = cheerio.load(htmlString);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
-    return out;
+    var outStr = JSON.stringify(out, null, 4);
+    console.log(outStr);
 };
 
 var clone = function(fn) {
@@ -62,14 +65,23 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-if(require.main == module) {
+if (require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url to download from')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (!program.url) {
+	checkHtml(loadHtmlFile(program.file), program.checks);
+    } else {
+	rest.get(program.url).on('complete', function(result, response) {
+            if (result instanceof Error) {
+		console.error('Error: ' + util.format(response.message));
+            } else {
+		checkHtml(result, program.checks);
+            }
+	});
+    }	
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
